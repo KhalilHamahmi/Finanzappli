446a7ba3d052d1c22d8980af7a2a2f43fa6a412d
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-06-05 10:49:53 +0200

    Create GrossRegionenDaten.sql

---
+++ doc/Datenbank/GrossRegionenDaten.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Grossregion" ("name", "miete", "einkommen", "p10", "p25", "p75", "p90") VALUES
+('Zürich (ZH)',                                1650, 7502, 4761, 5400, 10200, 13970),
+('Nordwestschweiz (BS, BL, AG, SO)',           1480, 7156, 4689, 5300, 9900,  12918),
+('Zentralschweiz (LU, UR, SZ, OW, NW, ZG)',   1450, 7092, 4745, 5250, 9700,  12856),
+('Genferseeregion (GE, VD, VS)',               1800, 6998, 4585, 5150, 9600,  12636),
+('Mittelland (BE, FR, NE, JU)',                1350, 6964, 4737, 5200, 9500,  11734),
+('Ostschweiz (SG, TG, AR, AI, GL, SH, GR)',   1300, 6623, 4563, 5000, 9100,  11030),
+('Tessin (TI)',                                1220, 5708, 3783, 4400, 7900,  10012);
